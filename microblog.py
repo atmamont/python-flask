b0a56152ca3280f538b69648de3b0b5c6cceedd2
Andrei Mamchenko <atmamont@gmail.com>
2021-04-13 17:55:16 +0200

    Setup shell shortcuts

--- microblog.py
+++ microblog.py
@@ -1,7 +1,7 @@
 # from flask import redirect, url_for, session
 from app import app, db
+from app.models import User, Post
 
-
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
