6bcc8c41eed5ab0381d22404e28dd9a0601dd374
Andrei Mamchenko <atmamont@gmail.com>
2021-04-15 20:17:30 +0200

    Add login using database and hashed pass

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import redirect, url_for, render_template, session, flash
-from app import app, db
+from flask import redirect, url_for, render_template, flash, request
+from app import app
 from app.models import User
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 @app.route("/user", methods=["POST", "GET"])
@@ -16,31 +18,28 @@ def home():
 
 
 @app.route("/view")
+# @login_required
 def view():
     return render_template("view.html", values=User.query.all())
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
     if form.validate_on_submit():
-        user = form.username.data
-        session["user"] = user
-
-        found_user = User.query.filter_by(username=user).first()
-        if found_user:
-            session["email"] = found_user.email
-        else:
-            usr = User()
-            usr.username = user
-
-            db.session.add(usr)
-            db.session.commit()
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid name or password")
+            redirect(url_for("login"))
 
         flash(f"Logged in {user} successfully", "info")
-        return redirect(url_for("user"))
-
-    if "user" in session:
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('user')        
         return redirect(url_for("user"))
 
     return render_template("login.html", form=form)
@@ -48,8 +47,6 @@ def login():
 
 @app.route("/logout")
 def logout():
-    session.pop("user", None)
-    session.pop("email", None)
-
+    logout_user()
     flash("Logged out successfully", "info")
     return redirect(url_for("login"))
